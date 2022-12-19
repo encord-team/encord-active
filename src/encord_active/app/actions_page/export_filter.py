da91ac9caff9ae9498d3ba1bc02392aa989e8af5
David Sapiro <david.sapiro@encord.com>
2022-12-19 16:02:39 +0000

    refactor: unifies db queries for state population

--- src/encord_active/app/actions_page/export_filter.py
+++ src/encord_active/app/actions_page/export_filter.py
@@ -12,7 +12,7 @@ from pandas.api.types import (
 
 import encord_active.app.common.state as state
 from encord_active.app.common.action_utils import create_new_project_on_encord_platform
-from encord_active.app.common.utils import load_merged_df, set_page_config, setup_page
+from encord_active.app.common.utils import set_page_config, setup_page
 from encord_active.lib.coco.encoder import generate_coco_file
 from encord_active.lib.db.tags import Tags
 
