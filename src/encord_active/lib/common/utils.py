42903711385ffb45c6c376357848e1bd617ce857
David Sapiro <david.sapiro@encord.com>
2022-12-28 11:01:06 +0000

    refactor: move cli helper to lib

--- src/encord_active/lib/common/utils.py
+++ src/encord_active/lib/common/utils.py
@@ -54,6 +54,17 @@ class ProjectNotFound(Exception):
         super().__init__(f"Couldn't find meta file for project in `{project_dir}`")
 
 
+def get_local_project(project_dir: Path) -> Project:
+    project_meta = fetch_project_meta(project_dir)
+
+    ssh_key_path = Path(project_meta["ssh_key_path"])
+    with open(ssh_key_path.expanduser(), "r", encoding="utf-8") as f:
+        key = f.read()
+
+    client = EncordUserClient.create_with_ssh_private_key(key)
+    return client.get_project(project_meta.get("project_hash"))
+
+
 def fetch_project_meta(data_dir: Path) -> ProjectMeta:
     meta_file = data_dir / "project_meta.yaml"
     if not meta_file.is_file():
