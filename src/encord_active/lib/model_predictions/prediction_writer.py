1691345c06921128be582213b257bea158b92fe0
Gorkem Polat <gorkem@encord.com>
2023-01-04 11:09:10 +0000

    fix: check np dtype

--- src/encord_active/lib/model_predictions/prediction_writer.py
+++ src/encord_active/lib/model_predictions/prediction_writer.py
@@ -65,7 +65,12 @@ def polyobj_to_nparray(o: dict, width: int, height: int) -> np.ndarray:
 
 def points_to_mask(points: np.ndarray, width: int, height: int):
     mask = np.zeros((height, width), dtype=np.uint8)
-    mask = cv2.fillPoly(mask, [(points * np.array([[width, height]])).astype(int)], 1)  # type: ignore
+
+    if np.issubdtype(points.dtype, np.float):
+        if not (np.min(points) >= 0.0 and np.max(points) <= 1.0):
+            raise ValueError("Float polygon points between 0 and 1.")
+        points = points * np.array([[width, height]])
+    mask = cv2.fillPoly(mask, [(points).astype(int)], 1)  # type: ignore
     return mask
 
 
