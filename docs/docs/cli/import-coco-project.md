ce3b6f008047ee2ae82be31d16f17689ea0c36ba
Frederik Hvilshøj <93145535+frederik-encord@users.noreply.github.com>
2023-01-09 10:31:12 +0000

    feat: local project import (#67)
    * feat(coco): make coco import local
    
    feat(coco): allow using symlinks
    
    * refactor(project-import): print next step for both coco and encord projects
    
    * fix: linting issues

--- docs/docs/cli/import-coco-project.md
+++ docs/docs/cli/import-coco-project.md
@@ -11,7 +11,7 @@ Make sure you have installed Encord Active with the `coco` [extras](/installatio
 If you already have a project on your local machine using the COCO data format, you can import that project with the following command:
 
 ```shell
-encord-active import project --coco ./images ./annotations.json
+encord-active import project --coco -i ./images -a ./annotations.json
 ```
 
 This will create a new Encord Active project inside a new directory in you current working directory.
@@ -33,21 +33,21 @@ coco-project-foo
 You can provide any path to each of the arguments as long as the first one is a directory of images and the second is an annotations file following the [COCO data format](https://cocodataset.org/#format-data).
 :::
 
-If this is your first time using Encord Active, you will need the path to your private `ssh-key` associated with Encord.
-Don't worry! The script will guide you on the way if you don't know it already.
-
 Running the importer will do the following things.
 
-1. Create a dataset.
-2. Create an ontology.
-3. Create a project.
-4. Create a local Encord Active project.
-5. Compute all the [metrics](category/metrics).
+1. Create a local Encord Active project.
+2. Compute all the [metrics](category/metrics).
 
 :::info
 
-- Steps 1-3 are on the Encord platform. This is a dependency that we intend to make optional in the future.
-- Step 4 will make a hard copy of the images used in your dataset. In the future, we expect to support symlinks or just pointers to the original images.
+Step 1 will by default make a hard copy of the images used in your dataset.
+**Optionally**, you can add the `--symlinks` argument
+
+```shell
+encord-active import project --coco -i ./images -a ./annotations.json --symlinks
+```
+
+to tell Encord Active to use symlinks instead of copying files. But be aware that **if you later move or delete your original image files, Encord Active will stop working!**
 
 :::
 
@@ -55,3 +55,9 @@ The whole flow might take a while depending on the size of your dataset.
 Bare with us, it is worth the wait.
 
 When the process is done, follow the printed instructions to open the app or see more details in the [Open Encord Active](/cli/open-encord-active) page.
+
+:::info
+
+If you want the dataset to be available on the Encord platform, please refer to the [Export documentation](/pages/export/filter_export#export-to-encord).
+
+:::
--- src/encord_active/cli/imports.py
+++ src/encord_active/cli/imports.py
@@ -2,7 +2,10 @@ import pickle
 from pathlib import Path
 from typing import Optional
 
+import rich
 import typer
+from rich.markup import escape
+from rich.panel import Panel
 
 from encord_active.cli.config import app_config
 from encord_active.cli.utils.decorators import ensure_project
@@ -44,43 +47,89 @@ def import_predictions(
     app_import_predictions(project, target, predictions)
 
 
+ENCORD_RICH_PANEL = "Encord Project Arguments"
+COCO_RICH_PANEL = "COCO Project Arguments"
+REQUIRED_WITH_COCO = f"[dark_red]{escape('[required with --coco]')}[/dark_red]"
+
+
 @import_cli.command(name="project")
 def import_project(
-    images_dir: Optional[Path] = typer.Argument(
+    target: Path = typer.Option(
+        Path.cwd(),
+        "--target",
+        "-t",
+        help="Directory where the project would be saved.",
+        file_okay=False,
+    ),
+    # Encord
+    encord_project_hash: Optional[str] = typer.Option(
         None,
-        help="Path to the directory containing the dataset images. (only required with `--cooc`)",
+        "--project_hash",
+        help="Encord project hash of the project you wish to import. Leaving it blank will allow you to choose one interactively.",
+        rich_help_panel=ENCORD_RICH_PANEL,
+    ),
+    # COCO
+    coco: bool = typer.Option(False, help="Import a project from the coco format", rich_help_panel=COCO_RICH_PANEL),
+    images: Optional[Path] = typer.Option(
+        None,
+        "--images",
+        "-i",
+        help=f"Path to the directory containing the dataset images. {REQUIRED_WITH_COCO}",
         file_okay=False,
         exists=True,
+        rich_help_panel=COCO_RICH_PANEL,
     ),
-    annotations_file: Optional[Path] = typer.Argument(
+    annotations: Optional[Path] = typer.Option(
         None,
-        help="Path to the file containing the dataset annotations. (only required with `--cooc`)",
+        "--annotations",
+        "-a",
+        help=f"Path to the file containing the dataset annotations. {REQUIRED_WITH_COCO}",
         dir_okay=False,
         exists=True,
+        rich_help_panel=COCO_RICH_PANEL,
     ),
-    encord_project_hash: Optional[str] = typer.Option(
-        None, "--project_hash", help="Encord project hash of the project you wish to import."
+    symlinks: bool = typer.Option(
+        False,
+        help="Use symlinks instead of copying COCO images to the target directory.",
+        rich_help_panel=COCO_RICH_PANEL,
     ),
-    target: Path = typer.Option(
-        Path.cwd(), "--target", "-t", help="Directory where the project would be saved.", file_okay=False
-    ),
-    coco: bool = typer.Option(False, help="Import a project from the coco format"),
 ):
     """
     [bold]Imports[/bold] a new project from Encord or a local coco project 📦
+
+    Confirm with each help panel what information you will have to provide.
     """
     if coco:
         from encord_active.cli.utils.coco import import_coco_project
 
-        if not annotations_file:
-            raise typer.BadParameter("`annotations_file` argument is missing")
-        elif not images_dir:
-            raise typer.BadParameter("`images_dir` argument is missing")
+        if not annotations:
+            raise typer.BadParameter("`annotations` argument is missing")
+        elif not images:
+            raise typer.BadParameter("`images` argument is missing")
 
-        ssh_key_path = app_config.get_or_query_ssh_key()
-        import_coco_project(images_dir, annotations_file, target, ssh_key_path)
+        project_path = import_coco_project(images, annotations, target, use_symlinks=symlinks)
     else:
         from encord_active.cli.utils.encord import import_encord_project
 
         ssh_key_path = app_config.get_or_query_ssh_key()
-        import_encord_project(ssh_key_path, target, encord_project_hash)
+        project_path = import_encord_project(ssh_key_path, target, encord_project_hash)
+
+    cwd = Path.cwd()
+    cd_dir = project_path.relative_to(cwd) if project_path.is_relative_to(cwd) else project_path
+
+    panel = Panel(
+        f"""
+    The data is downloaded and the metrics are complete.
+
+    You can run
+
+    [cyan]cd "{escape(cd_dir.as_posix())}"
+    encord-active visualise[/cyan]
+
+    to open Encord Active and see your project.
+    """,
+        title="🌟 Success 🌟",
+        style="green",
+        expand=False,
+    )
+    rich.print(panel)
