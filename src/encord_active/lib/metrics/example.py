35783c85b22145bb27c681b0e4cdd10cb8feb208
Eloy Pérez Torres <99720527+eloy-encord@users.noreply.github.com>
2023-01-18 14:48:48 +0000

    fix(example-metric): update name of `perform_test` (#90)

--- src/encord_active/lib/metrics/example.py
+++ src/encord_active/lib/metrics/example.py
@@ -64,7 +64,7 @@ if __name__ == "__main__":
     import sys
     from pathlib import Path
 
-    from encord_active.lib.metric.execute import perform_test
+    from encord_active.lib.metrics.execute import execute_metric
 
     path = sys.argv[1]
-    perform_test(ExampleMetric(), data_dir=Path(path))
+    execute_metric(ExampleMetric(), data_dir=Path(path))
