b4524bf08e81bf40607bbdb1c019286b1166207f
Gorkem Polat <gorkem@encord.com>
2023-01-19 16:00:13 +0000

    fix: get correct median value

--- src/encord_active/lib/dataset/summary_utils.py
+++ src/encord_active/lib/dataset/summary_utils.py
@@ -1,22 +1,25 @@
-import numpy as np
-from pathlib import Path
 import json
+from pathlib import Path
+
+import numpy as np
 from encord.constants.enums import DataType
-from typing import List
+
+
 def get_all_image_sizes(project_folder: Path) -> np.ndarray:
     image_sizes = []
-    for label_row in (project_folder / 'data').iterdir():
-        if (label_row / 'label_row.json').exists():
-            label_row_meta = json.loads((label_row / 'label_row.json').read_text(encoding='utf-8'))
-            if label_row_meta['data_type'] in [DataType.IMAGE.value, DataType.IMG_GROUP.value]:
-                for data_unit in label_row_meta['data_units'].values():
-                    image_sizes.append([data_unit['width'], data_unit['height']])
+    for label_row in (project_folder / "data").iterdir():
+        if (label_row / "label_row.json").exists():
+            label_row_meta = json.loads((label_row / "label_row.json").read_text(encoding="utf-8"))
+            if label_row_meta["data_type"] in [DataType.IMAGE.value, DataType.IMG_GROUP.value]:
+                for data_unit in label_row_meta["data_units"].values():
+                    image_sizes.append([data_unit["width"], data_unit["height"]])
 
-    return  np.array(image_sizes)
+    return np.array(image_sizes)
 
-def get_median_value_of_2D_array(array: np.ndarray)-> np.ndarray:
-    areas = array[:, 0]*array[:, 1]
-    areas.sort()
-    median_value = areas[areas.size//2]
-    item_index = np.where(areas==median_value)
+
+def get_median_value_of_2D_array(array: np.ndarray) -> np.ndarray:
+    areas = array[:, 0] * array[:, 1]
+    areas_sorted = np.sort(areas)
+    median_value = areas_sorted[areas_sorted.size // 2]
+    item_index = np.where(areas == median_value)
     return array[item_index[0][0], :]
