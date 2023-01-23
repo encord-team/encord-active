854d113ca8cdd99a0cd6b20e36a16d050df2daed
Frederik Hvilshøj <93145535+frederik-encord@users.noreply.github.com>
2023-01-23 14:22:14 +0100

    feat: fuzzy project search for encord projects (#106)
    * feat: fuzzy project search for encord projects
    
    * fix: liniting
    
    * fix: documentation

--- src/encord_active/lib/encord/utils.py
+++ src/encord_active/lib/encord/utils.py
@@ -1,11 +1,17 @@
 from json import dumps as json_dumps
 from pathlib import Path
-from typing import Optional
+from typing import List, Optional
 
 from encord import EncordUserClient
+from encord.orm.project import Project
+
+
+def get_encord_projects(ssh_key_path: Path, query: Optional[str] = None) -> List[Project]:
+    client = EncordUserClient.create_with_ssh_private_key(ssh_key_path.read_text())
+    projects: List[Project] = list(map(lambda x: x["project"], client.get_projects(title_like=query)))
+    return projects
 
 
 def get_projects_json(ssh_key_path: Path, query: Optional[str] = None) -> str:
-    client = EncordUserClient.create_with_ssh_private_key(ssh_key_path.read_text())
-    projects = map(lambda x: x["project"], client.get_projects(title_like=query))
+    projects = get_encord_projects(ssh_key_path, query)
     return json_dumps({p.project_hash: p.title for p in projects}, indent=2)
