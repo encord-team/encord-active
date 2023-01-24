85420ede6de84201610d7f3bc7ad69cb2f209585
Frederik Hvilshøj <frederik@cord.tech>
2023-01-24 15:32:44 +0100

    feat(cli): add links to resources

---
+++ src/encord_active/lib/constants.py
@@ -0,0 +1,6 @@
+DOCS_URL = "https://encord-active-docs.web.app"
+GITHUB_URL = "https://github.com/encord-team/encord-active"
+SLACK_INVITE_URL = "https://join.slack.com/t/encordactive/shared_invite/zt-1hc2vqur9-Fzj1EEAHoqu91sZ0CX0A7Q"
+SLACK_URL = "https://encordactive.slack.com"
+
+ENCORD_EMAIL = "active@encord.com"
