c6d86a570d76203f1bb98da73e0e7871362284a1
Gorkem Polat <gorkem@encord.com>
2023-01-25 12:00:50 +0000

    feat: mark undersampled label types

--- src/encord_active/lib/charts/data_quality_summary.py
+++ src/encord_active/lib/charts/data_quality_summary.py
@@ -25,24 +25,50 @@ def create_outlier_distribution_chart(
         )
     )
     fig.update_layout(
+        margin=dict(l=0, r=0, b=0),
         title_text="Outliers", height=400, legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
     )
 
     return fig
 
 def create_labels_distribution_chart(labels: dict, title: str, x_title: str='Class', y_title: str='Count') -> go.Figure:
-    object_df = pd.DataFrame.from_dict(labels, orient='index').reset_index()
-    object_df.rename(columns={'index': x_title, 0: y_title}, inplace=True)
+    labels_df = pd.DataFrame.from_dict(labels, orient='index').reset_index()
+    labels_df.rename(columns={'index': x_title, 0: y_title}, inplace=True)
 
-    object_df.sort_values(by=y_title, ascending=False, inplace=True)
+    labels_df.sort_values(by=y_title, ascending=False, inplace=True)
 
-    fig = px.bar(object_df, x = x_title, y=y_title)
+    Q2 = labels_df[y_title].quantile(0.5)
+
+    labels_df['undersampled'] = False
+    labels_df.loc[labels_df[y_title]<= (Q2*0.5), 'undersampled'] = True
+
+    fig = go.Figure(data=[go.Bar(
+        x=labels_df.loc[labels_df['undersampled']==False][x_title],
+        y=labels_df[labels_df['undersampled']==False][y_title],
+        name='representative',
+        marker_color='#3380FF'
+    ),
+        go.Bar(
+            x=labels_df.loc[labels_df['undersampled'] == True][x_title],
+            y=labels_df[labels_df['undersampled'] == True][y_title],
+            name='undersampled',
+            marker_color='tomato'
+        )
+    ])
+
     fig.update_layout(
+        margin=dict(l=0, r=0, b=0),
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1),
         title=title,
         xaxis_title=x_title,
         yaxis_title=y_title,
-        height=300,
     )
+
+
+
+
+
+
 
     return fig
 
