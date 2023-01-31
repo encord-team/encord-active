cd000582d850b7730602c960810c40e8dfd9502b
David Sapiro <david.sapiro@encord.com>
2023-01-31 15:50:05 +0000

    feat(encord-client): adds a 5 retry default

--- src/encord_active/lib/encord/utils.py
+++ src/encord_active/lib/encord/utils.py
@@ -3,11 +3,18 @@ from pathlib import Path
 from typing import List, Optional
 
 from encord import EncordUserClient
+from encord.http.constants import RequestsSettings
 from encord.orm.project import Project
 
 
+def get_client(ssh_key_path: Path):
+    return EncordUserClient.create_with_ssh_private_key(
+        ssh_key_path.read_text(encoding="utf-8"), requests_settings=RequestsSettings(max_retries=5)
+    )
+
+
 def get_encord_projects(ssh_key_path: Path, query: Optional[str] = None) -> List[Project]:
-    client = EncordUserClient.create_with_ssh_private_key(ssh_key_path.read_text())
+    client = get_client(ssh_key_path)
     projects: List[Project] = list(map(lambda x: x["project"], client.get_projects(title_like=query)))
     return projects
 
