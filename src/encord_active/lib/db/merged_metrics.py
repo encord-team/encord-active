b0346cc06ca7d24173f3b466814f70cb8443fa97
Yogesh Agarwal <yogesh@encord.com>
2023-02-08 18:08:04 +0000

    unused import

--- src/encord_active/lib/db/merged_metrics.py
+++ src/encord_active/lib/db/merged_metrics.py
@@ -4,7 +4,6 @@ from typing import List
 
 import pandas as pd
 from encord.project_ontology.classification_type import ClassificationType
-from pydantic import ValidationError
 
 from encord_active.lib.db.connection import DBConnection
 from encord_active.lib.db.tags import Tag, TagScope
