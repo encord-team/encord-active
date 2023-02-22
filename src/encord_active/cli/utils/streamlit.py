a89b2eda4d257a1a7230a62afd6178dcca687660
Frederik Hvilshøj <93145535+frederik-encord@users.noreply.github.com>
2023-02-22 08:34:28 +0100

    chore: disable file watchers when not in development mode (#165)
    * chore: disable file watchers when not in development mode
    
    * docs: add ea config tip to contributing docs

--- src/encord_active/cli/utils/streamlit.py
+++ src/encord_active/cli/utils/streamlit.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 from streamlit.web import cli as stcli
 
+from encord_active.app.app_config import app_config
+
 
 def launch_streamlit_app(target: Path):
     streamlit_page = (Path(__file__).parents[2] / "app" / "streamlit_entrypoint.py").expanduser().absolute()
@@ -12,4 +14,9 @@ def launch_streamlit_app(target: Path):
 
     # NOTE: we need to set PYTHONPATH for file watching
     environ["PYTHONPATH"] = (Path(__file__).parents[2]).as_posix()
+
+    if app_config.is_dev:
+        sys.argv.insert(3, "--server.fileWatcherType")
+        sys.argv.insert(4, "auto")
+
     sys.exit(stcli.main())  # pylint: disable=no-value-for-parameter
