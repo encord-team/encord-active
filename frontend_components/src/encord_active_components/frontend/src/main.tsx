b38748b206195dd0588452284282637839bde3ed
David Sapiro <david.sapiro@encord.com>
2023-03-03 15:49:31 +0000

    chore: restructure frontend components for extendability

--- frontend_components/src/encord_active_components/frontend/src/main.tsx
+++ frontend_components/src/encord_active_components/frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { PagesMenu } from "./components";
+import { EncordActiveComponents } from "./components";
 import "./index.css";
 
 import { StreamlitProvider } from "./streamlit/StreamlitProvider";
@@ -8,7 +8,7 @@ import { StreamlitProvider } from "./streamlit/StreamlitProvider";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <StreamlitProvider>
-      <PagesMenu />
+      <EncordActiveComponents />
     </StreamlitProvider>
   </React.StrictMode>
 );
