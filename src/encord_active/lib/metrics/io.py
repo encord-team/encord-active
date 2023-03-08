4e5142d62d9e0edba5678770e21baeb2beea4448
Eloy Pérez Torres <eloy@encord.com>
2023-03-08 10:08:06 +0100

    misc: move important logs to errors

--- src/encord_active/lib/metrics/io.py
+++ src/encord_active/lib/metrics/io.py
@@ -72,13 +72,13 @@ def load_module(module_path: Union[str, Path]):
 
     # Load the module from its full path
     if module_path.suffix != ".py":
-        logger.warning(f"Module '{module_path.as_posix()}' doesn't have a valid python module extension (py).")
+        logger.error(f"Module '{module_path.as_posix()}' doesn't have a valid python module extension (py).")
         return None
     try:
         spec = importlib.util.spec_from_file_location(module_path.stem, module_path.as_posix())
         mod = importlib.util.module_from_spec(spec)  # type: ignore
         spec.loader.exec_module(mod)  # type: ignore
     except Exception as e:
-        logger.warning(f"Module '{module_path.as_posix()}' is ill-formed. Exception: {e}")
+        logger.error(f"Module '{module_path.as_posix()}' is ill-formed. Exception: {e}")
         return None
     return mod
