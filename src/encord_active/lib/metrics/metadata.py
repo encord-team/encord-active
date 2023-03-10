e4881cd6e0fd10cd397460ae82a0f0fa8022d754
Frederik Hvilshøj <frederik@cord.tech>
2023-03-10 10:17:16 +0100

    fix: create metrics directory if not exists

--- src/encord_active/lib/metrics/metadata.py
+++ src/encord_active/lib/metrics/metadata.py
@@ -13,4 +13,5 @@ def fetch_metrics_meta(project_dir: Path):
 
 def update_metrics_meta(project_dir: Path, updated_meta: dict):
     meta_file_path = project_dir / "metrics" / "metrics_meta.json"
+    meta_file_path.parent.mkdir(exist_ok=True)
     meta_file_path.write_text(json.dumps(updated_meta, indent=2), encoding="utf-8")
