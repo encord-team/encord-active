6b6d66f74355b6eab9f6f8361448e49be28aee23
Gorkem Polat <gorkem@encord.com>
2023-03-10 15:56:12 +0000

    fix: change data frame schema

--- src/encord_active/lib/model_predictions/classification_metrics.py
+++ src/encord_active/lib/model_predictions/classification_metrics.py
@@ -10,13 +10,13 @@ from encord_active.lib.model_predictions.reader import (
 def match_predictions_and_labels(
     model_predictions: DataFrame[ClassificationPredictionSchema], labels: DataFrame[ClassificationLabelSchema]
 ) -> DataFrame[ClassificationPredictionMatchSchema]:
-    model_predictions = model_predictions.copy()
-    labels = labels.copy()
+    _model_predictions = model_predictions.copy()
+    _labels = labels.copy()
 
-    model_predictions[ClassificationPredictionMatchSchema.is_true_positive] = (
-        model_predictions[ClassificationPredictionMatchSchema.class_id]
-        .eq(labels[ClassificationLabelSchema.class_id])
+    _model_predictions[ClassificationPredictionMatchSchema.is_true_positive] = (
+        _model_predictions[ClassificationPredictionSchema.class_id]
+        .eq(_labels[ClassificationLabelSchema.class_id])
         .astype(float)
     )
 
-    return model_predictions.pipe(ClassificationPredictionMatchSchema)
+    return _model_predictions.pipe(ClassificationPredictionMatchSchema)
