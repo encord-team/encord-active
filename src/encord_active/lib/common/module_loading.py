c5ac475dc24d7f2f6acecbc7f6b4c5ed3e6c626b
Frederik Hvilshøj <frederik@cord.tech>
2023-03-20 14:28:53 +0100

    fix: stop early with bad transformer file and raise error on module load

--- src/encord_active/lib/common/module_loading.py
+++ src/encord_active/lib/common/module_loading.py
@@ -2,7 +2,15 @@ import importlib.util
 from pathlib import Path
 from typing import Union
 
-from loguru import logger
+
+class ModuleLoadError(Exception):
+    def __init__(self, e: Exception, module_path: Path) -> None:
+        self.module_path = module_path
+        self.e = e
+        super().__init__(str(self))
+
+    def __str__(self) -> str:
+        return f"Failed to load module '{self.module_path}' with error {self.e}"
 
 
 def load_module(module_path: Union[str, Path]):
@@ -11,13 +19,11 @@ def load_module(module_path: Union[str, Path]):
 
     # Load the module from its full path
     if module_path.suffix != ".py":
-        logger.error(f"Module '{module_path.as_posix()}' doesn't have a valid python module extension (py).")
-        return None
+        raise ValueError(f"Module '{module_path.as_posix()}' doesn't have a valid python module extension (py).")
     try:
         spec = importlib.util.spec_from_file_location(module_path.stem, module_path.as_posix())
         mod = importlib.util.module_from_spec(spec)  # type: ignore
         spec.loader.exec_module(mod)  # type: ignore
     except Exception as e:
-        logger.error(f"Module '{module_path.as_posix()}' is ill-formed. Exception: {e}")
-        return None
+        raise ModuleLoadError(e, module_path)
     return mod
