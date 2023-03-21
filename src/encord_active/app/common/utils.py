2dca4d3f6d01688337f8109ca4c5ab9571ccbc5e
Frederik Hvilshøj <93145535+frederik-encord@users.noreply.github.com>
2023-03-21 11:52:02 +0100

    feat(export): show filter stats (#266)
    * feat(export): filter stats on filter and export page
    
    * fix: comments from pr

--- src/encord_active/app/common/utils.py
+++ src/encord_active/app/common/utils.py
@@ -1,4 +1,5 @@
 import mimetypes
+from math import floor, log
 from pathlib import Path
 
 import streamlit as st
@@ -24,3 +25,17 @@ def set_page_config():
 
 def setup_page():
     write_page_css()
+
+
+def human_format(number: int) -> str:
+    units = ["", "K", "M", "G", "T", "P"]
+    k = 1000.0
+
+    if number == 0:
+        return "0"
+
+    magnitude = int(floor(log(abs(number), k)))
+
+    unit = units[magnitude]
+    fmt = "{:.2f}{}" if unit else "{:.0f}{}"
+    return fmt.format(number / k**magnitude, units[magnitude])
