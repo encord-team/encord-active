442f983494c15e7719f51d6ccac3a9b7dc19411b
Eloy Pérez Torres <eloy@encord.com>
2023-03-24 15:45:49 +0100

    feat: add MNIST train sandbox dataset

--- src/encord_active/lib/project/sandbox_projects.py
+++ src/encord_active/lib/project/sandbox_projects.py
@@ -48,6 +48,11 @@ PREBUILT_PROJECTS: dict[str, PrebuiltProject] = {
         hash="14a52852-55f6-46c9-850a-40e11540605f",
         name="[open-source]-TACO-Unofficial",
     ),
+    "[open-source][train]-mnist-dataset": PrebuiltProject(
+        url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D%5Btrain%5D-mnist-dataset.zip",
+        hash="34413f3b-fed5-4a34-a279-b68a0c5fe325",
+        name="[open-source][train]-mnist-dataset",
+    ),
     "[open-source][test]-mnist-dataset": PrebuiltProject(
         url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D%5Btest%5D-mnist-dataset.zip",
         hash="5c96df58-beac-4e42-a74d-fce16622c5af",
