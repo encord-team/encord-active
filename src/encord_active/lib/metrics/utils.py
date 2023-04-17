d75d62a3c642a1bce3ebc63a2bc3542a3c989ac0
David Sapiro <david.sapiro@encord.com>
2023-04-17 14:07:21 +0100

    fix: temp hack to move the object group metrics to the data explorer

--- src/encord_active/lib/metrics/utils.py
+++ src/encord_active/lib/metrics/utils.py
@@ -96,15 +96,19 @@ def get_metric_operation_level(pth: Path) -> str:
     return "O" if object_hashes else "F"
 
 
-def is_valid_annotation_type(
-    annotation_type: List[AnnotationTypeUnion], metric_scope: Optional[MetricScope] = None
-) -> bool:
-    if metric_scope == MetricScope.DATA_QUALITY:
-        return not annotation_type
-    elif metric_scope == MetricScope.LABEL_QUALITY:
-        return bool(annotation_type) and isinstance(annotation_type, list)
-    else:
+def is_valid_annotation_type(metric: MetricMetadata, metric_scope: Optional[MetricScope] = None) -> bool:
+    if not metric_scope:
         return True
+
+    if metric.title in ["Object Count", "Frame object density"]:
+        return metric_scope == MetricScope.DATA_QUALITY
+
+    if metric_scope == MetricScope.DATA_QUALITY:
+        return not metric.annotation_type
+    elif metric_scope == MetricScope.LABEL_QUALITY:
+        return bool(metric.annotation_type) and isinstance(metric.annotation_type, list)
+    else:
+        return False
 
 
 def load_metric_metadata(meta_pth) -> MetricMetadata:
@@ -160,7 +164,7 @@ def load_available_metrics(metric_dir: Path, metric_scope: Optional[MetricScope]
         return out
 
     for p, n, m, l in zip(paths, names, meta_data, levels):
-        if m is None or not l or not is_valid_annotation_type(m.annotation_type, metric_scope):
+        if m is None or not l or not is_valid_annotation_type(m, metric_scope):
             continue
 
         out.append(MetricData(name=n, path=p, meta=m, level=l))  # type: ignore
