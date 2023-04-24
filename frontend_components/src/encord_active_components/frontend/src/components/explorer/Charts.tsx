b3ef5a4f93aeba7b2d4264dffa7cb800f13eb2b0
David Sapiro <david.sapiro@encord.com>
2023-04-24 13:49:22 +0100

    feat: tiny selectable distribution chart

--- frontend_components/src/encord_active_components/frontend/src/components/explorer/Charts.tsx
+++ frontend_components/src/encord_active_components/frontend/src/components/explorer/Charts.tsx
@@ -1,44 +1,121 @@
-import { Histogram, Scatter, ScatterConfig } from "@ant-design/plots";
-import { useCallback } from "react";
-import { Item2DEmbedding } from "./api";
+import {
+  Column,
+  ColumnConfig,
+  Scatter,
+  ScatterConfig,
+  Tooltip,
+} from "@ant-design/plots";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import { IdValue, Item2DEmbedding } from "./api";
+import { bin } from "d3-array";
 
 const BINS = 20;
 
-export const MetricDistribution = ({ values }: { values: number[] }) => {
-  const max = Math.max(...values);
-  const min = Math.min(...values);
-  const binSize = (max - min) / BINS || 1;
+export const MetricDistributionTiny = ({
+  values,
+  setSeletedIds,
+}: {
+  values: IdValue[];
+  setSeletedIds: (ids: string[]) => void;
+}) => {
+  const { bins, columns } = useMemo(() => {
+    const bins = bin<IdValue, number>()
+      .thresholds(BINS)
+      .value((v) => v.value)(values);
+    const columns = bins.map((items, bin) => ({ value: items.length, bin }));
+    return { bins, columns };
+  }, [JSON.stringify(values)]);
+
+  const [selectedBins, setSelectedBins] = useState<number[]>([]);
+
+  useEffect(() => {
+    if (!values.length) return;
+    setSeletedIds(
+      selectedBins.length
+        ? selectedBins.flatMap((bin) => bins[bin].map(({ id }) => id))
+        : values.map(({ id }) => id)
+    );
+  }, [selectedBins]);
+
+  const onEvent = useCallback<NonNullable<ColumnConfig["onEvent"]>>(
+    (_, { type, view }) => {
+      if ("mouseup" === type)
+        setSelectedBins(
+          // @ts-ignore
+          (view.filteredData as typeof columns).map(({ bin }) => bin)
+        );
+    },
+    []
+  );
+
+  const customContent = useCallback<NonNullable<Tooltip["customContent"]>>(
+    (_, hoveredElements) => {
+      if (!hoveredElements.length || !hoveredElements[0].data) return null;
+      const { x0, x1 } = bins[hoveredElements[0].data.bin];
+
+      return (
+        <div className="flex flex-col items-center gap-1 py-2">
+          <div className="inline-flex justify-between gap-1 w-full">
+            <span>{x0}</span>
+            <span>,</span>
+            <span>{x1}</span>
+          </div>
+          <span>Count: {hoveredElements[0].data.value}</span>
+        </div>
+      );
+    },
+    [bins]
+  );
 
   return (
-    <Histogram
-      data={values.map((value) => ({ value }))}
-      binField="value"
-      binWidth={binSize}
-      meta={{ range: { min, tickInterval: ~~binSize } }}
+    <Column
+      className="w-full max-w-xs"
+      autoFit={true}
+      data={columns}
+      columnWidthRatio={1}
+      yField={"value"}
+      xField={"bin"}
+      xAxis={false}
+      yAxis={false}
+      brush={
+        bins.length > 1
+          ? {
+              enabled: true,
+              type: "x-rect",
+              action: "filter",
+              mask: {
+                style: { fill: "rgba(255,0,0,0.15)" },
+              },
+            }
+          : {}
+      }
+      onEvent={onEvent}
+      tooltip={{
+        customContent,
+      }}
     />
   );
 };
 
-const SELECTION_EVENTS = [
-  "mouseup",
-  "brush-reset-button:click",
-  "legend-item:click",
-];
+const fixedFormatter = (value: number) => value.toFixed(2);
 
 export const ScatteredEmbeddings = ({
   embeddings,
   onSelectionChange,
+  onReset,
 }: {
   embeddings: Item2DEmbedding[];
   onSelectionChange: (ids: Item2DEmbedding[]) => void;
+  onReset: () => void;
 }) => {
   const onEvent = useCallback<NonNullable<ScatterConfig["onEvent"]>>(
     (_, { type, view }) => {
-      if (SELECTION_EVENTS.includes(type))
+      if (["mouseup", "legend-item:click"].includes(type))
         // @ts-ignore
         onSelectionChange(view.filteredData as Item2DEmbedding[]);
+      else if (type === "brush-reset-button:click") onReset();
     },
-    []
+    [JSON.stringify(embeddings)]
   );
 
   return (
@@ -50,11 +127,16 @@ export const ScatteredEmbeddings = ({
       size={3}
       shape="circle"
       pointStyle={{ fillOpacity: 1 }}
+      interactions={[{ type: "reset-button", enable: false }]}
       brush={{
         enabled: true,
         mask: {
           style: { fill: "rgba(255,0,0,0.15)" },
         },
+      }}
+      meta={{
+        x: { formatter: fixedFormatter },
+        y: { formatter: fixedFormatter },
       }}
       onEvent={onEvent}
     />
