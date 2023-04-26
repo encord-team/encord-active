bd250692accb9859e9209d11f17dabda45455086
David Sapiro <david.sapiro@encord.com>
2023-04-26 13:04:02 +0100

    fix: removes concurency and downstreams project safty ensurance

--- src/encord_active/cli/utils/streamlit.py
+++ src/encord_active/cli/utils/streamlit.py
@@ -5,9 +5,21 @@ from pathlib import Path
 from streamlit.web import cli as stcli
 
 from encord_active.app.app_config import app_config
+from encord_active.cli.utils.decorators import find_child_projects, is_project
+from encord_active.lib.db.merged_metrics import ensure_initialised_merged_metrics
+from encord_active.lib.versioning.git import GitVersioner
+
+
+def ensure_safe_project(path: Path):
+    paths = [path] if is_project(path) else find_child_projects(path)
+
+    for path in paths:
+        GitVersioner(path).jump_to("latest")
+        ensure_initialised_merged_metrics(path)
 
 
 def launch_streamlit_app(target: Path):
+    ensure_safe_project(target)
     streamlit_page = (Path(__file__).parents[2] / "app" / "streamlit_entrypoint.py").expanduser().absolute()
     data_dir = target.expanduser().absolute().as_posix()
     sys.argv = ["streamlit", "run", streamlit_page.as_posix(), data_dir]
