0cfe1dd91a2933d718b74de034cbcbb69c9809a3
Eloy Pérez Torres <eloy@encord.com>
2023-04-26 16:04:49 +0200

    misc(pylint): add utf-8 encoding to decorator

--- src/encord_active/lib/common/decorators.py
+++ src/encord_active/lib/common/decorators.py
@@ -7,7 +7,7 @@ def silence_stdout(fn):
     @wraps(fn)
     def wrapper(*args, **kwargs):
         stdout_orig = sys.stdout
-        sys.stdout = open(os.devnull, "w")
+        sys.stdout = open(os.devnull, "w", encoding="utf-8")
         fn(*args, **kwargs)
         sys.stdout = stdout_orig
 
