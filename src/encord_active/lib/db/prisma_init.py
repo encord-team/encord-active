10bedb27abaf3d1e2803480d2607f34a09533b31
David Sapiro <david.sapiro@encord.com>
2023-05-02 13:37:52 +0100

    fix: handle non migrated databases

--- src/encord_active/lib/db/prisma_init.py
+++ src/encord_active/lib/db/prisma_init.py
@@ -14,7 +14,10 @@ PRISMA_SCHEMA_FILE = Path(__file__).parent / "schema.prisma"
 
 def ensure_prisma_db(db_path: Path):
     env = {"MY_DATABASE_URL": f"file:{db_path}"}
-    prisma_run(["migrate", "deploy", f"--schema={PRISMA_SCHEMA_FILE}"], env=env)
+    failure = prisma_run(["migrate", "deploy", f"--schema={PRISMA_SCHEMA_FILE}"], env=env)
+    # TODO: remove me before release
+    if failure:
+        prisma_run(["migrate", "reset", "--force", "--skip-generate", f"--schema={PRISMA_SCHEMA_FILE}"], env=env)
 
 
 def generate_prisma_client():
