971f6b1e54ff798a67cb2f2665504db4506c807c
Eloy Pérez Torres <eloy@encord.com>
2023-05-04 16:40:27 +0200

    misc: enable export to encord feature

--- src/encord_active/app/actions_page/export_filter.py
+++ src/encord_active/app/actions_page/export_filter.py
@@ -436,8 +436,10 @@ def render_export_button(
     render_col.button(
         "🏗 Export to Encord",
         on_click=lambda: current_form.set(CurrentForm.EXPORT),  # type: ignore
-        disabled=True,
-        help="Contact Encord",
+        disabled=not action_utils or is_filtered,
+        help="Export to an Encord dataset and project"
+        if not is_filtered
+        else "Export is allowed only for entire datasets, create a subset first or remove all filters",
     )
     if current_form.value == CurrentForm.EXPORT:
         df = get_state().merged_metrics
