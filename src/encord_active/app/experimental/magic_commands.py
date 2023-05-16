7e9b875fb26721cd22b4599a13e068b91cf0fd82
Gorkem Polat <gorkem@encord.com>
2023-05-16 15:26:21 +0100

    feat: add model_performance search

--- src/encord_active/app/experimental/magic_commands.py
+++ src/encord_active/app/experimental/magic_commands.py
@@ -1,7 +1,7 @@
 import configparser
 from datetime import date
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 
 import matplotlib.pyplot as plt
 import openai
@@ -10,14 +10,29 @@ import streamlit as st
 
 from encord_active.app.common.state import get_state
 from encord_active.app.common.utils import setup_page
+from encord_active.app.model_quality.prediction_type_builder import (
+    ModelQualityPage,
+    PredictionTypeBuilder,
+)
 from encord_active.app.model_quality.prediction_types.classification_type_builder import (
     ClassificationTypeBuilder,
 )
+from encord_active.app.model_quality.prediction_types.object_type_builder import (
+    ObjectTypeBuilder,
+)
 from encord_active.lib.metrics.metadata import fetch_metrics_meta
 
+# Merged metrics column name
 image_id = "image_id"
 data_type = "data_type"
 object_id = "object_id"
+
+# Classification predictions column name
+correct_prediction = "prediction"
+ground_truth_class_name = "ground truth class name"
+predicted_class_name = "predicted class name"
+
+# ChatGPT parameters
 START_CODE_TAG = "START_CODE"
 END_CODE_TAG = "END_CODE"
 INSTRUCT_TEMP = 0.4
@@ -32,7 +47,12 @@ This is the result of `print(df.head())`:
 {object_id} represents the name of each object, if it is None, it means that row represents an image.
 {data_type} represent weather the values in other columns belong to the image or object in that image.
 
-Return the python code (do not import anything) and make sure to prefix the requested python code with {START_CODE_TAG} exactly and suffix the code with {END_CODE_TAG} exactly to get the answer to the following question. The code script between prefix {START_CODE_TAG} and suffix {END_CODE_TAG} should be directly evaluated with python's builtin exec method:
+Return the python code (do not import anything) and make sure to prefix the requested python code with 
+{START_CODE_TAG} and suffix the code with {END_CODE_TAG} to get the answer to the following question. 
+The code script between prefix {START_CODE_TAG} and suffix {END_CODE_TAG} should be directly evaluated with 
+python's builtin exec method. 
+Do not create a new dataframe, assume that it is already created. 
+The last line of the code should print the result:
 """
 
 error_correction_message = """
@@ -53,13 +73,61 @@ You generated this python code:
 It fails with the following error:
 {error_returned}
 
-Correct the python code and return a new python code (do not import anything) that fixes the above mentioned error. Do not generate the same code again.
-Make sure to prefix the requested python code with {START_CODE_TAG} exactly and suffix the code with {END_CODE_TAG} exactly.
+Return the python code (do not import anything) and make sure to prefix the requested python code with 
+{START_CODE_TAG} and suffix the code with {END_CODE_TAG} to get the answer to the question. 
+The code script between prefix {START_CODE_TAG} and suffix {END_CODE_TAG} should be directly evaluated with 
+python's builtin exec method. 
+Do not create a new dataframe, assume that it is already created. 
+The last line of the code should print the result:
+"""
+
+instruct_message_classification_performance = """Today is {today_date}.
+You are provided with a pandas dataframe (df) with {df_num_rows} rows and {df_num_cols} columns.
+This is the result of `print(df.head())`:
+{df_head}.
+Each row corresponds to the properties of a single image. `{correct_prediction}` column indicates if the class prediction for 
+the image is true (1) or false (0) prediction. `{ground_truth_class_name}` indicates the label of the image, and `{predicted_class_name}` 
+indicates predicted class.
+
+Return the python code (do not import anything) and make sure to prefix the requested python code with 
+{START_CODE_TAG} and suffix the code with {END_CODE_TAG} to get the answer to the following question. 
+The code script between prefix {START_CODE_TAG} and suffix {END_CODE_TAG} should be directly evaluated with 
+python's builtin exec method. 
+Do not create a new dataframe, assume that it is already created. 
+The last line of the code should print the result:
+"""
+
+error_correction_message_classification_performance = """
+Today is {today_date}.
+You are provided with a pandas dataframe (df) with {df_num_rows} rows and {df_num_cols} columns.
+This is the result of `print(df.head())`:
+{df_head}.
+Each row corresponds to the properties of a single image. `{correct_prediction}` column indicates if the prediction of
+that column is true (1) or false (0). `{ground_truth_class_name}` indicates the label of the image, and `{predicted_class_name}` 
+indicates predicted class name.
+
+The user asked the following question:
+{question}
+
+You generated this python code:
+{code}
+
+It fails with the following error:
+{error_returned}
+
+Return the python code (do not import anything) and make sure to prefix the requested python code with 
+{START_CODE_TAG} and suffix the code with {END_CODE_TAG} to get the answer to the question. 
+The code script between prefix {START_CODE_TAG} and suffix {END_CODE_TAG} should be directly evaluated with 
+python's builtin exec method. 
+Do not create a new dataframe, assume that it is already created. 
+The last line of the code should print the result:
 """
 
 
 def magic_commands():
-    def _get_chatgpt_response(instruct: str, question: str, start_code_tag: str, end_code_tag: str, temperature) -> str:
+    def _get_chatgpt_response(
+        instruct: str, question: str, start_code_tag: str, end_code_tag: str, temperature: Optional[float] = None
+    ) -> str:
         config = configparser.ConfigParser()
         config.read(Path(__file__).parent / "keys.ini")
         openai.api_key = config.get("KEY", "OPENAI_API_KEY")
@@ -74,7 +142,10 @@ def magic_commands():
         if question != "":
             messages.append({"role": "user", "content": question})
 
-        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, temperature=temperature)
+        if temperature is not None:
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages, temperature=temperature)
+        else:
+            response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
 
         result = response["choices"][0]["message"]["content"]
         result = result.replace(start_code_tag, "")
@@ -130,7 +201,7 @@ def magic_commands():
                     question=magic_prompt,
                     start_code_tag=START_CODE_TAG,
                     end_code_tag=END_CODE_TAG,
-                    temperature=0.2,
+                    temperature=INSTRUCT_TEMP,
                 )
 
                 count = 0
@@ -161,7 +232,6 @@ def magic_commands():
                             question="",
                             start_code_tag=START_CODE_TAG,
                             end_code_tag=END_CODE_TAG,
-                            temperature=0.6,
                         )
 
                 if valid_code:
@@ -173,6 +243,7 @@ def magic_commands():
                     if return_object is None:
                         st.write("The generated code could not produce output")
                     else:
+                        st.markdown("#### Answer")
                         st.write(return_object)
                 else:
                     st.write("A code could not be generated for this prompt")
@@ -180,10 +251,90 @@ def magic_commands():
                 st.write("please provide a prompt to see the result")
 
         with model_performance_tab:
-            folders = [item.name for item in list(get_state().project_paths.predictions.glob("*"))]
-            if "classification" in folders:
-                st.write("\u2713 Classification predictions exist")
-                classification_performance_builder = ClassificationTypeBuilder()
-                st.write(classification_performance_builder._model_predictions)
+            classification_performance_builder = ClassificationTypeBuilder()
+            if classification_performance_builder.is_available():
+                classification_performance_builder.load_data(ModelQualityPage.METRICS)
+                df = classification_performance_builder._model_predictions.copy()
+
+                # Make it a bit more verbose for the LLM
+                df = df.rename(
+                    columns={
+                        "class_name": predicted_class_name,
+                        "gt_class_name": ground_truth_class_name,
+                        "is_true_positive": correct_prediction,
+                    }
+                )
+
+                st.write("Top 5 rows")
+                st.dataframe(df.head(5))
+                magic_prompt = st.text_input("What do you want to get?", key="prompt_classification_performance")
+
+                if magic_prompt != "":
+
+                    # code_to_run = get_code
+
+                    code_to_run = _get_chatgpt_response(
+                        instruct=instruct_message_classification_performance.format(
+                            today_date=date.today(),
+                            df_num_rows=df.shape[0],
+                            df_num_cols=df.shape[1],
+                            df_head=df.head(),
+                            correct_prediction=correct_prediction,
+                            ground_truth_class_name=ground_truth_class_name,
+                            predicted_class_name=predicted_class_name,
+                            START_CODE_TAG=START_CODE_TAG,
+                            END_CODE_TAG=END_CODE_TAG,
+                        ),
+                        question=magic_prompt,
+                        start_code_tag=START_CODE_TAG,
+                        end_code_tag=END_CODE_TAG,
+                        temperature=INSTRUCT_TEMP,
+                    )
+
+                    count = 0
+                    valid_code = False
+                    while count < TOTAL_TRIAL:
+                        try:
+                            exec(code_to_run, {"pd": pd, "df": df})
+                            valid_code = True
+                            break
+                        except Exception as e:
+                            count += 1
+                            code_to_run = _get_chatgpt_response(
+                                instruct=error_correction_message_classification_performance.format(
+                                    today_date=date.today(),
+                                    df_num_rows=df.shape[0],
+                                    df_num_cols=df.shape[1],
+                                    df_head=df.head(),
+                                    all_metrics=all_metrics,
+                                    correct_prediction=correct_prediction,
+                                    ground_truth_class_name=ground_truth_class_name,
+                                    predicted_class_name=predicted_class_name,
+                                    question=magic_prompt,
+                                    code=code_to_run,
+                                    error_returned=e,
+                                    START_CODE_TAG=START_CODE_TAG,
+                                    END_CODE_TAG=END_CODE_TAG,
+                                ),
+                                question="",
+                                start_code_tag=START_CODE_TAG,
+                                end_code_tag=END_CODE_TAG,
+                            )
+
+                    if valid_code:
+                        st.write(f"Generated code for this prompt (in {count + 1}. attempt):")
+                        st.code(code_to_run, language="python", line_numbers=True)
+
+                        return_object = _capture_return_object(code_to_run, df)
+
+                        if return_object is None:
+                            st.write("The generated code could not produce output")
+                        else:
+                            st.markdown("#### Answer")
+                            st.write(return_object)
+                    else:
+                        st.write(f"Tried {count} times, however a code could not be generated for this prompt :(")
+                else:
+                    st.write("please provide a prompt to see the result")
 
     return render
