eebce8d289fa89245f00cfdc8066edfa2d8fdb13
David Sapiro <david.sapiro@encord.com>
2023-05-22 17:38:03 +0300

    feat(sandbox): allows starting ea with a subset of projects

--- src/encord_active/lib/project/sandbox_projects/sandbox_projects.py
+++ src/encord_active/lib/project/sandbox_projects/sandbox_projects.py
@@ -10,6 +10,8 @@ from encord_active_components.components.projects_page import ProjectStats
 from rich.markup import escape
 from tqdm.auto import tqdm
 
+from encord_active.server.settings import AvailableSandboxProjects
+
 
 class PrebuiltProject(TypedDict):
     url: str
@@ -21,21 +23,13 @@ class PrebuiltProject(TypedDict):
 
 IMAGES_PATH = (Path(__file__).parent).resolve() / "images"
 
-# GCP bucket links will be added here
-PREBUILT_PROJECTS: dict[str, PrebuiltProject] = {
+BASE_PROJECTS: dict[str, PrebuiltProject] = {
     "[open-source][validation]-coco-2017-dataset": PrebuiltProject(
         url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D%5Bvalidation%5D-coco-2017-dataset.zip",
         hash="f2140a72-c644-4c31-be66-3ef80b3718e5",
         name="[open-source][validation]-coco-2017-dataset",
         image_path=(IMAGES_PATH / "coco.jpeg"),
         stats=ProjectStats(dataUnits=4952, labels=41420, classes=80),
-    ),
-    "[open-source][test]-limuc-ulcerative-colitis-classification": PrebuiltProject(
-        url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D%5Btest%5D-limuc-ulcerative-colitis-classification.zip",
-        hash="aa2b21bd-6f2e-48fc-8f4f-4ba4d9b7bd67",
-        name="[open-source][test]-limuc-ulcerative-colitis-classification",
-        image_path=(IMAGES_PATH / "limuc.png"),
-        stats=ProjectStats(dataUnits=1686, labels=1686, classes=4),
     ),
     "[open-source]-covid-19-segmentations": PrebuiltProject(
         url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D-covid-19-segmentations.zip",
@@ -50,27 +44,6 @@ PREBUILT_PROJECTS: dict[str, PrebuiltProject] = {
         name="[open-source][validation]-bdd-dataset",
         image_path=(IMAGES_PATH / "bdd.jpeg"),
         stats=ProjectStats(dataUnits=981, labels=12983, classes=8),
-    ),
-    "[open-source]-TACO-Official": PrebuiltProject(
-        url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D-TACO-Official.zip",
-        hash="dc1cf137-f1b9-4c2f-973d-32512c971955",
-        name="[open-source]-TACO-Official",
-        image_path=(IMAGES_PATH / "taco.jpeg"),
-        stats=ProjectStats(dataUnits=1500, labels=5038, classes=59),
-    ),
-    "[open-source]-TACO-Unofficial": PrebuiltProject(
-        url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D-TACO-Unofficial.zip",
-        hash="14a52852-55f6-46c9-850a-40e11540605f",
-        name="[open-source]-TACO-Unofficial",
-        image_path=(IMAGES_PATH / "taco_unofficial.jpeg"),
-        stats=ProjectStats(dataUnits=3731, labels=8419, classes=60),
-    ),
-    "[open-source][train]-mnist-dataset": PrebuiltProject(
-        url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D%5Btrain%5D-mnist-dataset.zip",
-        hash="34413f3b-fed5-4a34-a279-b68a0c5fe325",
-        name="[open-source][train]-mnist-dataset",
-        image_path=IMAGES_PATH / "mnist.png",
-        stats=ProjectStats(dataUnits=119000, labels=0, classes=0),
     ),
     "[open-source][test]-mnist-dataset": PrebuiltProject(
         url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D%5Btest%5D-mnist-dataset.zip",
@@ -93,6 +66,37 @@ PREBUILT_PROJECTS: dict[str, PrebuiltProject] = {
         image_path=(IMAGES_PATH / "quickstart.jpeg"),
         stats=ProjectStats(dataUnits=199, labels=1617, classes=71),
     ),
+}
+
+ADDITIONAL_PROJECTS: dict[str, PrebuiltProject] = {
+    "[open-source][test]-limuc-ulcerative-colitis-classification": PrebuiltProject(
+        url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D%5Btest%5D-limuc-ulcerative-colitis-classification.zip",
+        hash="aa2b21bd-6f2e-48fc-8f4f-4ba4d9b7bd67",
+        name="[open-source][test]-limuc-ulcerative-colitis-classification",
+        image_path=(IMAGES_PATH / "limuc.png"),
+        stats=ProjectStats(dataUnits=1686, labels=1686, classes=4),
+    ),
+    "[open-source]-TACO-Official": PrebuiltProject(
+        url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D-TACO-Official.zip",
+        hash="dc1cf137-f1b9-4c2f-973d-32512c971955",
+        name="[open-source]-TACO-Official",
+        image_path=(IMAGES_PATH / "taco.jpeg"),
+        stats=ProjectStats(dataUnits=1500, labels=5038, classes=59),
+    ),
+    "[open-source]-TACO-Unofficial": PrebuiltProject(
+        url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D-TACO-Unofficial.zip",
+        hash="14a52852-55f6-46c9-850a-40e11540605f",
+        name="[open-source]-TACO-Unofficial",
+        image_path=(IMAGES_PATH / "taco_unofficial.jpeg"),
+        stats=ProjectStats(dataUnits=3731, labels=8419, classes=60),
+    ),
+    "[open-source][train]-mnist-dataset": PrebuiltProject(
+        url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D%5Btrain%5D-mnist-dataset.zip",
+        hash="34413f3b-fed5-4a34-a279-b68a0c5fe325",
+        name="[open-source][train]-mnist-dataset",
+        image_path=IMAGES_PATH / "mnist.png",
+        stats=ProjectStats(dataUnits=119000, labels=0, classes=0),
+    ),
     "[open-source][train]-Caltech-101": PrebuiltProject(
         url="https://storage.googleapis.com/encord-active-sandbox-data/%5Bopen-source%5D%5Btrain%5D-Caltech-101.zip",
         hash="d083bd28-fd4a-4b58-a80d-a1a9074d2cdc",
@@ -110,6 +114,15 @@ PREBUILT_PROJECTS: dict[str, PrebuiltProject] = {
 }
 
 
+def available_prebuilt_projects(sandbox_projects: AvailableSandboxProjects = AvailableSandboxProjects.ALL):
+    if sandbox_projects == AvailableSandboxProjects.ALL:
+        return {**BASE_PROJECTS, **ADDITIONAL_PROJECTS}
+    elif sandbox_projects == AvailableSandboxProjects.BASE:
+        return BASE_PROJECTS
+    else:
+        return {}
+
+
 def fetch_prebuilt_project_size(project_name: str) -> Optional[float]:
     """
     Returns the storage size (MB) of a prebuilt project stored in the cloud
@@ -117,9 +130,9 @@ def fetch_prebuilt_project_size(project_name: str) -> Optional[float]:
     :param project_name: Prebuilt project name
     :return: Prebuilt project's storage size (MB). Can return None if the information is not available.
     """
-    if project_name not in PREBUILT_PROJECTS.keys():
+    if project_name not in available_prebuilt_projects().keys():
         return None
-    url = PREBUILT_PROJECTS[project_name]["url"]
+    url = available_prebuilt_projects()[project_name]["url"]
     with requests.head(url) as r:
         total_length = fetch_response_content_length(r)
         size = round(total_length / 1000000.0, 1) if total_length is not None else None
@@ -133,7 +146,7 @@ def fetch_response_content_length(r: requests.Response) -> Optional[int]:
 def fetch_prebuilt_project(
     project_name: str, out_dir: Path, *, unpack=True, progress_callback: Optional[Callable] = None
 ):
-    url = PREBUILT_PROJECTS[project_name]["url"]
+    url = available_prebuilt_projects()[project_name]["url"]
     output_file_name = "prebuilt_project.zip"
     output_file_path = out_dir / output_file_name
     rich.print(f"Output destination: {escape(out_dir.as_posix())}")
