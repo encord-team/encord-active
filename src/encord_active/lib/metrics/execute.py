caca90ef8343f7297a5ffd73cf4f028cffd03467
Frederik Hvilshøj <frederik@cord.tech>
2023-05-22 14:46:13 +0200

    fix: remove early faiss import

--- src/encord_active/lib/metrics/execute.py
+++ src/encord_active/lib/metrics/execute.py
@@ -7,9 +7,9 @@ from typing import Callable, List, Optional, Sequence, Tuple, Type, Union
 
 import cv2
 
-# NOTE: this statement ensures faiss is imported before torch.
+# NOTE: this statement ensures pytorch is imported before faiss.
 # removing it will trigger segmentation faults in certain metrics
-import faiss  # pylint: disable=unused-import
+import torch  # pylint: disable=unused-import
 from loguru import logger
 
 from encord_active.lib.common.iterator import DatasetIterator, Iterator
