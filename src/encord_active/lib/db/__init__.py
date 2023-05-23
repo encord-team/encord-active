b3dcf0c4416506ac4a1f14b04427bbe69b1f61e0
james-encord <james@encord.com>
2023-05-23 11:39:52 +0100

    Force quit on regenerate prisma message with request to re-execute the command again.

--- src/encord_active/lib/db/__init__.py
+++ src/encord_active/lib/db/__init__.py
@@ -5,6 +5,7 @@ def _regen():
     import importlib
     import logging
     logger = logging.getLogger("Prisma")
+    import sys
     try:
         import prisma
         import prisma.client
@@ -12,6 +13,9 @@ def _regen():
         if did_schema_change():
             generate_prisma_client()
             importlib.reload(prisma)
+            # Exit with message instead, as attempt at dynamic reload doesn't seem to be working.
+            logger.warning("Prisma db module is out of date, regenerating, please execute the command again")
+            sys.exit(1)
     except (RuntimeError, ImportError):
         generate_prisma_client()
         importlib.reload(prisma)  # pylint: disable=used-before-assignment
