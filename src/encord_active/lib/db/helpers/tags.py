eb04abe9cb9046dfa18ce55cc269bbb8f3ad5a5b
Frederik Hvilshøj <frederik@cord.tech>
2023-05-26 09:51:35 +0200

    feat: tag nested classifications

--- src/encord_active/lib/db/helpers/tags.py
+++ src/encord_active/lib/db/helpers/tags.py
@@ -2,6 +2,7 @@ from typing import Dict, List, Set, TypedDict
 
 import pandas as pd
 
+from encord_active.lib.common.iterator import DatasetIterator
 from encord_active.lib.db.connection import DBConnection
 from encord_active.lib.db.merged_metrics import MergedMetrics
 from encord_active.lib.db.tags import Tag, TagScope
@@ -53,3 +54,54 @@ def from_grouped_tags(tags: GroupedTags) -> tuple[List[Tag], List[Tag]]:
     data_tags = [Tag(tag, TagScope.DATA) for tag in tags["data"]]
     label_tags = [Tag(tag, TagScope.LABEL) for tag in tags["label"]]
     return data_tags, label_tags
+
+
+def populate_tags_with_nested_classifications(pfs: ProjectFileStructure, option_answer_hashes: set[str]):
+    with DBConnection(pfs) as conn:
+        mm = MergedMetrics(conn).all()
+    no_label_tag = Tag("no classification", TagScope.LABEL)
+
+    def update_label_tags(label: dict, base_key: str, identifier_key: str, answer_key: str, answer_dict: dict):
+        label_identifier = label[identifier_key]
+        label_hash = label[answer_key]
+        key = f"{base_key}_{label_identifier}"
+
+        if key not in mm.index:
+            return
+
+        tags = mm.loc[key].tags
+        new_tags: list[Tag] = []
+
+        for question in answer_dict[label_hash].get("classifications", [{}]):
+            if question["featureHash"] not in option_answer_hashes:
+                continue
+
+            name = question["name"]
+            for answer in question["answers"]:
+                value = answer["name"]
+                new_tags.append(Tag(f"{name}: {value}", TagScope.LABEL))
+
+        if not new_tags:
+            new_tags = [no_label_tag]
+        elif no_label_tag in tags:
+            tags.pop(tags.index(no_label_tag))
+
+        for tag in new_tags:
+            if tag in tags:
+                continue
+            tags.append(tag)
+
+        with DBConnection(pfs) as conn:
+            MergedMetrics(conn).update_tags(key, tags)
+
+    iterator = DatasetIterator(pfs.project_dir)
+    for du, _ in iterator.iterate(desc="Tagging Objects with nested classifications"):
+        base_key = f"{iterator.label_hash}_{iterator.du_hash}_{iterator.frame:05d}"
+        object_answers = iterator.label_rows[iterator.label_hash]["object_answers"]
+        classification_answers = iterator.label_rows[iterator.label_hash]["classification_answers"]
+
+        for clf in du.get("labels", {}).get("classifications", []):
+            update_label_tags(clf, base_key, "featureHash", "classificationHash", classification_answers)
+
+        for obj in du.get("labels", {}).get("objects", []):
+            update_label_tags(obj, base_key, "objectHash", "objectHash", object_answers)
