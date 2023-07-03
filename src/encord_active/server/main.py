f5cc08ac490f74852464d4f56b7fb37ce089aa0d
David Sapiro <david.sapiro@encord.com>
2023-07-03 16:38:42 +0100

    perf: pre-load the similiarity finders on startup

--- src/encord_active/server/main.py
+++ src/encord_active/server/main.py
@@ -3,10 +3,12 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
 from encord_active.cli.utils.decorators import find_child_projects, is_project
+from encord_active.lib.metrics.types import EmbeddingType
 from encord_active.lib.model_predictions.reader import read_prediction_files
 from encord_active.lib.model_predictions.writer import MainPredictionType
 from encord_active.lib.project.project_file_structure import ProjectFileStructure
 from encord_active.server.dependencies import verify_premium
+from encord_active.server.utils import get_similarity_finder
 
 from .routers import project
 from .settings import get_settings
@@ -34,8 +36,14 @@ async def on_startup():
     paths = [root_path] if is_project(root_path) else find_child_projects(root_path)
 
     for path in paths:
+        project_file_structure = ProjectFileStructure(path)
         for prediction_type in MainPredictionType:
-            read_prediction_files(ProjectFileStructure(path), prediction_type)
+            read_prediction_files(project_file_structure, prediction_type)
+        for embedding_type in EmbeddingType:
+            try:
+                get_similarity_finder(embedding_type, project_file_structure)
+            except:
+                pass
 
 
 @app.get("/premium_available")
