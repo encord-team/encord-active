b6aa17cfe2217a78f56ab41739e8b8335c7b32ef
James Clifton <james@encord.com>
2023-07-03 09:32:52 +0100

    Rename & unify object, classification.

--- src/encord_active/db/query.py
+++ src/encord_active/db/query.py
@@ -2,11 +2,11 @@ from typing import Union
 
 from sqlalchemy import func
 from sqlmodel import Session, select
-from .models import ProjectDataAnalytics, ProjectObjectAnalytics
+from .models import ProjectDataAnalytics, ProjectAnnotationAnalytics
 
 
 def analysis_count(session: Session, label: bool, project_hash: str) -> int:
-    ty = ProjectObjectAnalytics if label else ProjectDataAnalytics
+    ty = ProjectAnnotationAnalytics if label else ProjectDataAnalytics
     count: int = session.query(ty).with_entities(func.count()).where(ty.project_hash == project_hash).scalar()
     return count
 
@@ -20,7 +20,7 @@ def analysis_quartile(
 ) -> Union[float, int]:
     if not metric_name.startswith("metric_"):
         raise ValueError(f"Invalid metric name: {metric_name}")
-    ty = ProjectObjectAnalytics if label else ProjectDataAnalytics
+    ty = ProjectAnnotationAnalytics if label else ProjectDataAnalytics
     metric_attr = getattr(ty, metric_name)
     count: int = session.query(ty)\
         .with_entities(func.count())\
