e373e288555a87e9f14e1f32fd742012bb3a1500
Frederik Hvilshøj <93145535+frederik-encord@users.noreply.github.com>
2023-07-11 10:32:46 +0200

    fix: only embed on migration if ontology has entries (#536)

--- src/encord_active/lib/db/data/migrations/202306141156_add_emebedding_index.py
+++ src/encord_active/lib/db/data/migrations/202306141156_add_emebedding_index.py
@@ -1,8 +1,22 @@
+import orjson
+
 from encord_active.lib.embeddings.embedding_index import EmbeddingIndex
 from encord_active.lib.metrics.types import EmbeddingType
 from encord_active.lib.project.project_file_structure import ProjectFileStructure
 
 
+def project_has_embedding_type(pfs: ProjectFileStructure, et: EmbeddingType):
+    if et == EmbeddingType.IMAGE:
+        return True
+
+    ontology = orjson.loads(pfs.ontology.read_text())  # pylint: disable=no-member
+    if et == EmbeddingType.OBJECT:
+        return bool(ontology.get("objects", []))
+    if et == EmbeddingType.OBJECT:
+        return bool(ontology.get("classifications", []))
+
+
 def up(pfs: ProjectFileStructure):
     for embedding_type in [EmbeddingType.IMAGE, EmbeddingType.OBJECT, EmbeddingType.CLASSIFICATION]:
-        EmbeddingIndex.from_project(pfs, embedding_type)
+        if project_has_embedding_type(pfs, embedding_type):
+            EmbeddingIndex.from_project(pfs, embedding_type)
