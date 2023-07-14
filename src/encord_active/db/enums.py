c9429e5d390fcd1c4a5e779b4314f5dd83090516
James Clifton <james@encord.com>
2023-07-14 16:46:58 +0100

    Summary query filter support, part 3. Prediction implementation is partial and broken for the filtered use-case.

--- src/encord_active/db/enums.py
+++ src/encord_active/db/enums.py
@@ -1,11 +1,54 @@
+import enum
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, Optional, List
+
+
+class EnumType(enum.Enum):
+    ONTOLOGY = "ontology"
+    """Ontology well-known enum"""
+    ENUM = "enum"
+    """Simple enum with fixed values"""
 
 
 @dataclass
 class EnumDefinition:
-    pass
+    enum_type: EnumType
+    title: str
+    values: Optional[Dict[str, str]] = None,
+
+
+class AnnotationType(enum.Enum):
+    CLASSIFICATION = "classification"
+    BOUNDING_BOX = "bounding_box"
+    ROT_BOUNDING_BOX = "rot_bounding_box"
+    POINT = "point"
+    POLYLINE = "polyline"
+    POLYGON = "polygon"
+    SKELETON = "skeleton"
+    BITMASK = "bitmask"
 
 
 DataEnums: Dict[str, EnumDefinition] = {}
-AnnotationEnums: Dict[str, EnumDefinition] = {}
+AnnotationEnums: Dict[str, EnumDefinition] = {
+    "feature_hash": EnumDefinition(
+        enum_type=EnumType.ONTOLOGY,
+        title="Label Class",
+        values=None,
+    ),
+    "annotation_type": EnumDefinition(
+        enum_type=EnumType.ENUM,
+        title="Annotation Type",
+        values={
+            annotation_type.value: annotation_type.name
+            for annotation_type in AnnotationType
+        },
+    ),
+    "annotation_manual": EnumDefinition(
+        enum_type=EnumType.ENUM,
+        title="Manual Annotation",
+        values={
+            "0": "Automated Annotation",
+            "1": "Manual Annotation",
+        }
+    )
+}
