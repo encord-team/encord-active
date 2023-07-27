0dc32e8a26a0d4456db028b968ab23fd2ffc7a98
Gorkem Polat <gorkem@encord.com>
2023-07-27 12:07:36 +0100

    fix: update to new doc url

--- src/encord_active/app/app_config.py
+++ src/encord_active/app/app_config.py
@@ -56,7 +56,7 @@ class AppConfig:
 Encord Active needs to know the path to the [blue]private ssh key[/blue] which is associated with Encord.
 Don't know this? Please see our documentation on the topic to get more help.
 
-[blue]https://docs.encord.com/docs/annotate-public-keys#set-up-public-key-authentication[/blue]
+[yellow]https://docs.encord.com/docs/annotate-public-keys[/yellow]
             """,
             title="SSH Key Path",
             expand=False,
