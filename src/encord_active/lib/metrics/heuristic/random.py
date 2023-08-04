6c3b3a6a513f26b14884c286739a54dc71ad6b9b
Eloy Pérez Torres <99720527+eloy-encord@users.noreply.github.com>
2023-08-04 10:24:05 +0200

    misc: improve messaging in metrics (#559)

--- src/encord_active/lib/metrics/heuristic/random.py
+++ src/encord_active/lib/metrics/heuristic/random.py
@@ -1,9 +1,12 @@
 import numpy as np
+from loguru import logger
 
 from encord_active.lib.common.iterator import Iterator
 from encord_active.lib.metrics.metric import Metric
 from encord_active.lib.metrics.types import AnnotationType, DataType, MetricType
 from encord_active.lib.metrics.writer import CSVMetricWriter
+
+logger = logger.opt(colors=True)
 
 
 class RandomImageMetric(Metric):
@@ -15,17 +18,15 @@ class RandomImageMetric(Metric):
             doc_url="https://docs.encord.com/docs/active-data-quality-metrics#random-values-on-images",
             metric_type=MetricType.HEURISTIC,
             data_type=DataType.IMAGE,
-            annotation_type=[],
+            annotation_type=AnnotationType.NONE,
         )
 
     def execute(self, iterator: Iterator, writer: CSVMetricWriter):
-        valid_annotation_types = {annotation_type.value for annotation_type in self.metadata.annotation_type}
-
         for _ in iterator.iterate(desc="Assigning random values to images"):
             writer.write(np.random.uniform())
 
 
-class RandomeObjectMetric(Metric):
+class RandomObjectMetric(Metric):
     def __init__(self):
         super().__init__(
             title="Random Values on Objects",
@@ -44,8 +45,15 @@ class RandomeObjectMetric(Metric):
     def execute(self, iterator: Iterator, writer: CSVMetricWriter):
         valid_annotation_types = {annotation_type.value for annotation_type in self.metadata.annotation_type}
 
-        for data_unit, _ in iterator.iterate(desc="Assigning random values to objects"):
+        found_any = False
+        for data_unit, _ in iterator.iterate(desc="Searching for objects and assigning random scores"):
             for obj in data_unit["labels"].get("objects", []):
                 if not obj["shape"] in valid_annotation_types:
                     continue
+                found_any = True
                 writer.write(np.random.uniform(), obj)
+
+        if not found_any:
+            logger.info(
+                f"<yellow>[Skipping]</yellow> No object labels of types {{{', '.join(valid_annotation_types)}}}."
+            )
