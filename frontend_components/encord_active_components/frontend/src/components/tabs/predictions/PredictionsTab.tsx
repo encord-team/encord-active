1530c828080552005f7bc7a209cb108771db20a9
james-encord <133006388+james-encord@users.noreply.github.com>
2023-08-04 14:52:03 +0100

    cleanup: better prediction effect
    Co-authored-by: David Sapiro <115489098+Encord-davids@users.noreply.github.com>
    Signed-off-by: james-encord <133006388+james-encord@users.noreply.github.com>

--- frontend_components/encord_active_components/frontend/src/components/tabs/predictions/PredictionsTab.tsx
+++ frontend_components/encord_active_components/frontend/src/components/tabs/predictions/PredictionsTab.tsx
@@ -39,14 +39,11 @@ export function PredictionsTab(
 
   // Auto-select prediction if one exists & unselect if an invalid prediction hash is selected;
   useEffect(() => {
-    let internalPredictionHash = predictionHash;
-    const a = allPredictionOptions?.findIndex(
-      (v) => v.value === predictionHash
-    );
-    if (a === -1 || a === undefined) {
-      internalPredictionHash = undefined;
-    }
-    if (allPredictionOptions != null && allPredictionOptions.length >= 1 && internalPredictionHash == null) {
+    if (
+      allPredictionOptions &&
+      allPredictionOptions?.find((v) => v.value === predictionHash)
+    )
+      setPredictionHash(allPredictionOptions[0].value);
         setPredictionHash(allPredictionOptions[0].value);
     }
   }, [predictionHash, allPredictionOptions]);
