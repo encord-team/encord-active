d85c2af009ac33be9fc2b534d25dbe5f564aa014
Eloy Pérez Torres <eloy@encord.com>
2023-08-17 14:15:33 +0200

    chore: reflect the slack community in the CLI

--- src/encord_active/lib/constants.py
+++ src/encord_active/lib/constants.py
@@ -2,5 +2,7 @@ DOCS_URL = "https://docs.encord.com/docs/active-overview"
 GITHUB_URL = "https://github.com/encord-team/encord-active"
 JOIN_DISCORD_URL = "https://discord.gg/TU6yT7Uvx3"
 DISCORD_URL = "https://discord.gg/TU6yT7Uvx3"
+SLACK_INVITE_URL = "https://join.slack.com/t/encordactive/shared_invite/zt-1hc2vqur9-Fzj1EEAHoqu91sZ0CX0A7Q"
+SLACK_URL = "https://encordactive.slack.com"
 
 ENCORD_EMAIL = "active@encord.com"
