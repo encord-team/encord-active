1f7cbe7b83fdfa33358b1ce2cc99fa37a97e1bcc
Frederik Hvilshøj <frederik@cord.tech>
2023-08-21 13:21:40 +0200

    fix: read cache dir from environment variable

--- src/encord_active/lib/common/data_utils.py
+++ src/encord_active/lib/common/data_utils.py
@@ -22,7 +22,8 @@ _EXTRACT_FRAMES_CACHE: Dict[str, str] = {}
 
 class Directory:
     def __init__(self, subdir: str) -> None:
-        base = "/home/jupyter/iterative_cache"
+        base = os.environ.get("EA_CACHE_BASE", "/home/jupyter/iterative_cache")
+        print(base)
         self.name = f"{base}/{subdir}"
         Path(self.name).mkdir(exist_ok=True)
 
