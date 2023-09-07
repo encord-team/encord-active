b80f567b0e1c3650a89c0a1657b5d7359cad5035
David Sapiro <david.sapiro@encord.com>
2023-09-07 12:00:21 +0100

    consolidate openapi gen interactions

--- src/encord_active/server/openapi_gen.py
+++ src/encord_active/server/openapi_gen.py
@@ -10,6 +10,7 @@ def generate_openapi_fe_components() -> None:
     import subprocess
     import os
     from unittest.mock import MagicMock
+
     # Path lookup
     src_dir = Path(__file__).parent.parent.parent.resolve()
     fe_dir = src_dir.parent / "frontend_components" / "encord_active_components" / "frontend"
@@ -29,20 +30,11 @@ def generate_openapi_fe_components() -> None:
         )
     print("Generated openapi.json")
     # Locate openapi-generate-cli.sh
-    openapi_gen_root = src_dir.parent.parent
-    openapi_gen_path: Optional[Path] = None
-    for name in [
-        "openapi-generator-cli.sh",
-        "openapi_generator_cli.sh",
-        "openapi-generator-cli",
-        "openapi_generator_cli"
-    ]:
-        openapi_gen_candidate = openapi_gen_root / name
-        if openapi_gen_candidate.exists():
-            openapi_gen_path = openapi_gen_candidate
-    if openapi_gen_path is None:
-        raise RuntimeError(f"Cannot locate openapi-generate-cli.sh in {openapi_gen_root}")
-    print(f"Located openapi generator: {openapi_gen_path.as_posix()}")
+    openapi_gen_path = fe_dir / "node_modules" / ".bin" / "openapi-generator-cli"
+    if not openapi_gen_path.exists():
+        raise RuntimeError(
+            f"openapi-generator-cli doesn't exist, make sure you ran `npm install` in the fronted directory"
+        )
     openapi_fe_output = fe_dir / "src" / "openapi"
     prettier_module = fe_dir / "node_modules" / ".bin" / "prettier"
     command = [
@@ -55,15 +47,15 @@ def generate_openapi_fe_components() -> None:
         "-g",
         "typescript-axios",
         "--additional-properties=nullSafeAdditionalProps=true,supportsES6=true,useUnionTypes=true",
-        "--enable-post-process-file"
+        "--enable-post-process-file",
     ]
     command_env = {
         **os.environ,
         "TS_POST_PROCESS_FILE": f"{prettier_module.as_posix()} --write",
-        "OPENAPI_GENERATOR_VERSION": "7.0.0"
+        "OPENAPI_GENERATOR_VERSION": "7.0.0",
     }
     print(f"Running openapi generator: {' '.join(command)}")
-    subprocess.run(' '.join(command), shell=True, check=True, env=command_env)
+    subprocess.run(" ".join(command), shell=True, check=True, env=command_env)
 
 
 if __name__ == "__main__":
