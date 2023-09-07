7794db1bd8caa0476138f3c612a9e14785e52ef4
Frederik Hvilshøj <frederik@cord.tech>
2023-09-07 13:14:49 +0200

    feat: flexible app port
    Set app port either with `PORT` env variable or with `--port` CLI argument

--- src/encord_active/cli/main.py
+++ src/encord_active/cli/main.py
@@ -9,6 +9,7 @@ from dotenv import load_dotenv
 from rich.markup import escape
 from rich.panel import Panel
 from typer.core import TyperGroup
+from typing_extensions import Annotated
 
 from encord_active.cli.project import project_cli
 from encord_active.cli.utils.server import ensure_safe_project
@@ -411,13 +412,14 @@ def start(
     target: Path = typer.Option(
         Path.cwd(), "--target", "-t", help="Path of the project you would like to start", file_okay=False
     ),
+    port: int = typer.Option(8000, help="Bind app to this port", envvar="PORT"),
 ):
     """
     [green bold]Launch[/green bold] the application with the provided project ✨
     """
     from encord_active.cli.utils.server import launch_server_app
 
-    launch_server_app(target)
+    launch_server_app(target, port)
 
 
 @cli.command()
@@ -425,6 +427,7 @@ def quickstart(
     target: Path = typer.Option(
         Path.cwd(), "--target", "-t", help="Directory where the project would be saved.", file_okay=False
     ),
+    port: int = typer.Option(8000, help="Bind app to this port", envvar="PORT"),
 ):
     """
     [green bold]Start[/green bold] Encord Active straight away 🏃💨
@@ -437,7 +440,7 @@ def quickstart(
     project_dir.mkdir(exist_ok=True)
 
     fetch_prebuilt_project(project_name, project_dir)
-    launch_server_app(project_dir)
+    launch_server_app(project_dir, port)
 
 
 @cli.command(rich_help_panel="Resources")
