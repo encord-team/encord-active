9bb82242f32ac24404c16adb0b84472c2e6775d3
Gorkem Polat <gorkem@encord.com>
2023-10-06 10:57:50 +0100

    fix: convert similarity to distance

--- src/encord_active/analysis/metrics/active_learning/uniqueness.py
+++ src/encord_active/analysis/metrics/active_learning/uniqueness.py
@@ -29,4 +29,4 @@ class ImageUniqueness(DerivedMetric):
         else:
             # Clamp large distances to (unique)
             # FIXME: filter out nearby frames / same video?
-            return min(max(nearest_neighbours.similarities[0], -1.0), 1.0)
+            return min(max((1 - nearest_neighbours.similarities[0]) / 2, 0.0), 1.0)
