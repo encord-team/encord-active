5fd48ad957d0a0392c3b983b8d449db09053e483
Frederik Hvilshøj <frederik@cord.tech>
2023-12-05 18:05:31 +0000

    refactor: clean up data querying

--- src/encord_active/public/active_project.py
+++ src/encord_active/public/active_project.py
@@ -1,122 +1,104 @@
 from dataclasses import dataclass
-from typing import Any
+from pathlib import Path
+from typing import Union
 from uuid import UUID, uuid4
 
 import pandas as pd
 from encord.objects import OntologyStructure
-from sqlalchemy import MetaData, Table, create_engine, select, text
+from sqlalchemy import Integer
 from sqlalchemy.engine import Engine
-from sqlalchemy.sql import Select
-from sqlmodel import Session
-from sqlmodel import select as sqlmodel_select
+from sqlmodel import Session, select
 
-from encord_active.db.models import ProjectTag, ProjectTaggedDataUnit
+from encord_active.db.models import (
+    Project,
+    ProjectDataAnalytics,
+    ProjectPrediction,
+    ProjectPredictionAnalytics,
+    ProjectTag,
+    ProjectTaggedDataUnit,
+    get_engine,
+)
+
+_P = Project
+_T = ProjectTag
 
 
 @dataclass
 class DataUnitItem:
-    du_hash: str
+    du_hash: UUID
     frame: int
 
 
-def get_active_engine(path_to_db: str) -> Engine:
-    return create_engine(f"sqlite:///{path_to_db}")
+def get_active_engine(path_to_db: Union[str, Path]) -> Engine:
+    path = Path(path_to_db) if isinstance(path_to_db, str) else path_to_db
+    return get_engine(path, use_alembic=False)
 
 
 class ActiveProject:
     def __init__(self, engine: Engine, project_name: str):
         self._engine = engine
-        self._metadata = MetaData(bind=self._engine)
         self._project_name = project_name
 
-        active_project = Table("active_project", self._metadata, autoload_with=self._engine)
-        stmt = select(active_project.c.project_hash).where(active_project.c.project_name == f"{self._project_name}")
+        with Session(self._engine) as sess:
+            res = sess.exec(
+                select(_P.project_hash, _P.project_ontology).where(_P.project_name == project_name).limit(1)
+            ).first()
 
-        with self._engine.connect() as connection:
-            result = connection.execute(stmt).fetchone()
+            if res is None:
+                raise ValueError(f"Couldn't find project with name `{project_name}` in the DB.")
 
-            if result is not None:
-                self._project_hash = result[0]
-            else:
-                self._project_hash = None
+            self.project_hash, ontology = res
+            project_tuples = sess.exec(select(_T.name, _T.tag_hash).where(_T.project_hash == self.project_hash)).all()
 
-        with Session(engine) as sess:
-            self._existing_tags = {
-                tag.name: tag.tag_hash
-                for tag in sess.exec(
-                    sqlmodel_select(ProjectTag).where(ProjectTag.project_hash == self._project_hash)
-                ).all()
-            }
-            sess.commit()
+            # Assuming that there's just one prediction model
+            # FIXME: With multiple sets of model predictions, we should select the right UUID here
+            self._model_hash = sess.exec(
+                select(ProjectPrediction.prediction_hash)
+                .where(ProjectPrediction.project_hash == self.project_hash)
+                .limit(1)
+            ).first()
 
-    def _execute_statement(self, stmt: Select) -> Any:
-        with self._engine.connect() as connection:
-            result = connection.execute(stmt).fetchone()
+        self._existing_tags = dict(project_tuples)
+        self.ontology = OntologyStructure.from_dict(ontology)  # type: ignore
 
-            if result is not None:
-                return result[0]
-            else:
-                return None
-
+    # For backward compatibility
     def get_ontology(self) -> OntologyStructure:
-        active_project = Table("active_project", self._metadata, autoload_with=self._engine)
-
-        stmt = select(active_project.c.project_ontology).where(active_project.c.project_hash == f"{self._project_hash}")
-
-        return OntologyStructure.from_dict(self._execute_statement(stmt))
+        return self.ontology
 
     def get_prediction_metrics(self) -> pd.DataFrame:
-        active_project_prediction = Table("active_project_prediction", self._metadata, autoload_with=self._engine)
-        stmt = select(active_project_prediction.c.prediction_hash).where(
-            active_project_prediction.c.project_hash == f"{self._project_hash}"
-        )
+        if self._model_hash is None:
+            raise ValueError(f"Project with name {self._project_name} does not have any model predictions")
 
-        prediction_hash = self._execute_statement(stmt)
+        with Session(self._engine) as sess:
+            P = ProjectPredictionAnalytics
+            stmt = select(  # type: ignore
+                P.du_hash,
+                P.feature_hash,
+                P.metric_area,
+                P.metric_area_relative,
+                P.metric_aspect_ratio,
+                P.metric_brightness,
+                P.metric_contrast,
+                P.metric_sharpness,
+                P.metric_red,
+                P.metric_green,
+                P.metric_blue,
+                P.metric_label_border_closeness,
+                P.metric_label_confidence,
+                (P.feature_hash == P.match_feature_hash).cast(Integer).label("true_positive"),  # type: ignore
+            ).where(P.project_hash == self.project_hash, P.prediction_hash == self._model_hash)
 
-        active_project_prediction_analytics = Table(
-            "active_project_prediction_analytics", self._metadata, autoload_with=self._engine
-        )
-
-        stmt = select(
-            [
-                active_project_prediction_analytics.c.du_hash,
-                active_project_prediction_analytics.c.feature_hash,
-                active_project_prediction_analytics.c.metric_area,
-                active_project_prediction_analytics.c.metric_area_relative,
-                active_project_prediction_analytics.c.metric_aspect_ratio,
-                active_project_prediction_analytics.c.metric_brightness,
-                active_project_prediction_analytics.c.metric_contrast,
-                active_project_prediction_analytics.c.metric_sharpness,
-                active_project_prediction_analytics.c.metric_red,
-                active_project_prediction_analytics.c.metric_green,
-                active_project_prediction_analytics.c.metric_blue,
-                active_project_prediction_analytics.c.metric_label_border_closeness,
-                active_project_prediction_analytics.c.metric_label_confidence,
-                text(
-                    """CASE
-                        WHEN feature_hash == match_feature_hash THEN 1
-                        ELSE 0
-                    END AS true_positive
-                """
-                ),
-            ]
-        ).where(active_project_prediction_analytics.c.prediction_hash == prediction_hash)
-
-        with self._engine.begin() as conn:
-            df = pd.read_sql(stmt, conn)
+            df = pd.DataFrame(sess.exec(stmt).all())
+            df.columns = list(sess.exec(stmt).keys())
 
         return df
 
     def get_images_metrics(self) -> pd.DataFrame:
-        active_project_analytics_data = Table(
-            "active_project_analytics_data", self._metadata, autoload_with=self._engine
-        )
-        stmt = select(active_project_analytics_data).where(
-            active_project_analytics_data.c.project_hash == f"{self._project_hash}"
-        )
-
-        with self._engine.begin() as conn:
-            df = pd.read_sql(stmt, conn)
+        with Session(self._engine) as sess:
+            image_metrics = sess.exec(
+                select(ProjectDataAnalytics).where(ProjectDataAnalytics.project_hash == self.project_hash)
+            )
+            df = pd.DataFrame([i.dict() for i in image_metrics])
 
         return df
 
@@ -129,7 +111,7 @@ class ActiveProject:
             new_tag = ProjectTag(
                 tag_hash=tag_hash,
                 name=tag,
-                project_hash=self._project_hash,
+                project_hash=self.project_hash,
                 description="",
             )
             sess.add(new_tag)
@@ -146,7 +128,7 @@ class ActiveProject:
             for du_item in du_items:
                 sess.add(
                     ProjectTaggedDataUnit(
-                        project_hash=self._project_hash,
+                        project_hash=self.project_hash,
                         du_hash=du_item.du_hash,
                         frame=du_item.frame,
                         tag_hash=tag_hash,
